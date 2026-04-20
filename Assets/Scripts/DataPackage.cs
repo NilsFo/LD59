b7fd19de66d999afa487e1965709542fd2f955e7
Patrick Herbers <patrick.herbers@ruhr-uni-bochum.de>
2026-04-20 20:06:03 +0200

    Data Packages

---
+++ Assets/Scripts/DataPackage.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class DataPackage : MonoBehaviour
+{
+    public List<GameObject> dataPath;
+    private int targetIndex = 0;
+    public float speed = 1f;
+
+    private void Start()
+    {
+        if (dataPath.Count == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        var curTarget = dataPath[targetIndex].transform;
+        transform.position = Vector3.MoveTowards(transform.position, curTarget.position, speed * Time.deltaTime);
+        transform.LookAt(curTarget, Vector3.up);
+        if (Vector3.SqrMagnitude(transform.position - curTarget.position) < 0.01f)
+        {
+            targetIndex++;
+            if (targetIndex >= dataPath.Count)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
